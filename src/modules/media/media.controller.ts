c4d63183afccac2e600927b744b6f49eb822bf1f
Pavel Grebeniuk <pavel.g@2muchcoffee.com>
2021-03-15 11:07:45 +0200

    added public decorator to media controller

--- src/modules/media/media.controller.ts
+++ src/modules/media/media.controller.ts
@@ -1,8 +1,10 @@
 import { Controller, Get, Param, Res } from '@nestjs/common';
 import { join, parse } from 'path';
+import { Public } from '../shared/decorators/public.decorator';
 
 @Controller()
 export class MediaController {
+  @Public()
   @Get('files/:path(*)')
   async getFile(@Res() res: any, @Param('path') path: string) {
     const filePath = join(__dirname, '..', '..', '..', path);
